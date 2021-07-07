8c44c30b73800151204bc1d4aeb5a212ea4cb7ba
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-07-07 20:17:17 +0300

    add test dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,4 +22,5 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
