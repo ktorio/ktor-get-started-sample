cccc26e60a212728816ed9481c2ffa0846becd43
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-08-12 10:49:49 +0400

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ val logback_version: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.7.10"
     id("io.ktor.plugin") version "2.1.0"
 }
 
